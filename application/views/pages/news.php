ef1a31b404c9cc218e1cc190e6513d4d2e473c25
giddel05012 <giddel.lucion@yahoo.com>
2020-10-09 18:27:36 +0800

    added news contents

--- application/views/pages/news.php
+++ application/views/pages/news.php
@@ -4,7 +4,7 @@
 <div class="content-wrapper">
     <div class="row news">
         <div class="col-sm-2">
-            <img class="d-block img-fluid" src="./assets/projects/Adopt-a-Family.jpg" alt="Adopt-a-Family">
+            <img class="d-block img-fluid" src="./assets/news/Supports_DOH/1.jpg" alt="Adopt-a-Family">
         </div>
         <div class="col-sm-10">
             <a href="<?php echo base_url(); ?>news/1">Hotel Sogo supports DOH’s Anti-COVID-19 Campaign</a>
@@ -20,7 +20,7 @@
         <div class="col-sm-12 brLineNews"></div>
 
         <div class="col-sm-2">
-            <img class="d-block img-fluid" src="./assets/projects/Adopt-a-Family.jpg" alt="Adopt-a-Family">
+            <img class="d-block img-fluid" src="./assets/news/Rolls_out_approved/1.jpg" alt="Adopt-a-Family">
         </div>
         <div class="col-sm-10">
             <a href="<?php echo base_url(); ?>news/2">Hotel Sogo rolls out approved “New Normal” policies in all branches</a>
@@ -28,6 +28,42 @@
                 normal policies to be implemented in all of its branches nationwide in lieu of 
                 the announcement of General Community Quarantine (GCQ) and Modified Enhance 
                 Community Quarantine (ECQ) in some areas of Luzon.....</p>
+        </div>
+
+        <div class="col-sm-12 brLineNews"></div>
+
+        <div class="col-sm-2">
+            <img class="d-block img-fluid" src="./assets/news/Provide_60M_worth/1.jpg" alt="Adopt-a-Family">
+        </div>
+        <div class="col-sm-10">
+            <a href="<?php echo base_url(); ?>news/3">Hotel Sogo provides P60M worth of rooms to medical frontliners</a>
+            <p>With the prolonged Luzon-wide lockdown, Hotel Sogo realizes, even more, the importance of their core value 
+                –Pagmamalasakit. Even though a lot of businesses have closed down, Hotel Sogo continued its support to the 
+                frontliners and LGUs who risk fighting against COVID-19.....</p>
+        </div>
+
+        <div class="col-sm-12 brLineNews"></div>
+
+        <div class="col-sm-2">
+            <img class="d-block img-fluid" src="./assets/news/taal/1.jpg" alt="Adopt-a-Family">
+        </div>
+        <div class="col-sm-10">
+            <a href="<?php echo base_url(); ?>news/4">Hotel Sogo provides immediate help after Taal Volcano eruption</a>
+            <p>Sogo Cares, the Corporate Social Responsibility Program of Hotel Sogo, headed immediately to the south last 
+                January 15 to provide aid such as blankets, pillows, bath towels, new clothes, and mineral waters to more 
+                than 1,200 displaced families of Taal Volcano in Batangas.....</p>
+        </div>
+
+        <div class="col-sm-12 brLineNews"></div>
+
+        <div class="col-sm-2">
+            <img class="d-block img-fluid" src="./assets/news/Hotel_Like_no_other/1.jpg" alt="Adopt-a-Family">
+        </div>
+        <div class="col-sm-10">
+            <a href="<?php echo base_url(); ?>news/5">Hotel Sogo: A hotel like no other</a>
+            <p>For almost four months that the Philippines has been hit by a dreaded disease, the country has witnessed 
+                various events from the rising numbers of COVID-19 confirmed cases, death tolls, recovered patients, the 
+                heroism of frontliners, lockdown implementations, different situations of every Filipino family, and many more....</p>
         </div>
 
         <div class="col-sm-12 brLineNews"></div>
--- application/views/pages/news_page.php
+++ application/views/pages/news_page.php
@@ -10,7 +10,8 @@ if($title == 1){
     Upholding the “so clean…so good” tagline, Hotel Sogo will stay true to its trailblazing nature by providing fellow frontliners a clean and healthy facilities along with excellent hotel services.  <br><br>
     To find out more about this and other Hotel Sogo’s campaigns, visit our website at www.hotelsogo.com or like and follow us on Facebook page @HotelSogoOfficialPage, Twitter and Instagram @hotelsogo, and Youtube account @OfficialHotelSogo.
     ';
-    $image = '../assets/image_here.png';
+    $image1 = '../assets/news/Supports_DOH/1.jpg';
+    $image2 = '../assets/image_here.png';
 }else if($title == 2){
     $header = 'Hotel Sogo rolls out approved “New Normal” policies in all branches';
     $content = 'Hotel Sogo CEO Reynaldo Malaca announced that the company has prepared its new normal policies to be implemented in all of its branches nationwide in lieu of the announcement of General Community Quarantine (GCQ) and Modified Enhance Community Quarantine (ECQ) in some areas of Luzon.<br><br>
@@ -36,13 +37,91 @@ if($title == 1){
     Hotel Sogo will continue to keep track on the government’s safety protocols and will stay in giving its guests not only a So Clean So Good stay but, also So Safe. <br>
     To find out more about Hotel Sogo, visit its website at www.hotelsogo.com or like and follow us on Facebook page @HotelSogoOfficialPage, Twitter, and Instagram @hotelsogo, and Youtube account @OfficialHotelSogo.
     ';
-    $image = '../assets/image_here.png';
+    $image1 = '../assets/news/Rolls_out_approved/1.jpg';
+    $image2 = '../assets/news/Rolls_out_approved/2.jpg';
+}else if($title == 3){
+    $header = 'Hotel Sogo provides P60M worth of rooms to medical frontliners';
+    $content = 'With the prolonged Luzon-wide lockdown, Hotel Sogo realizes, even more, the importance of their core value –Pagmamalasakit. Even though a lot of businesses have closed down, Hotel Sogo continued its support to the frontliners and LGUs who risk fighting against COVID-19.  <br><br>
+
+    As of now Hotel Sogo, the largest hotel chain in the Philippines has provided over 850 hotel room accommodations with a value of PhP 60 million for the frontline medical workers from 11 hospitals in different key cities in the country. Ten Hotel Sogo branches are dedicated to doctors, nurses, paramedics, and other healthcare providers who need a place to rest and sleep until the end of the quarantine phase, for free. <br><br>
+    
+    Meanwhile, in cooperation with the Research Institute for Tropical Medicine (RITM), Hotel Sogo Alabang branch, just minutes away from RITM, is fully occupied by our fellow frontliners for easier access to safe accommodation and to get the right amount of rest from everyday’s fight against COVID-19. <br><br>
+    
+    It all started with the company’s coordination to Manila City Mayor Isko Moreno in providing temporary shelter for our healthcare workers particularly living outside the city since mass transportation is suspended. <br><br>
+    
+    “On behalf of everyone here in RITM, we would like to say thank you for the accommodations in Sogo. It’s meant a lot for us. We really appreciate this and it’s been so helpful. This is the time that the doctors can come and sleep, because in other accommodations we have only with bunk beds and mats; it’s been such a treat to be able to sleep in a private bed. And also, sleep is absolutely important for your immune system. So, we would like to say thank you because you are saving lives –saving our lives,” Dr. J.P. Prado of RITM shared in a Facebook post. <br><br>
+    
+    The hotel management has also been in touch with the local government of Muntinlupa, especially with Mayor Jaime Fresnedi, in providing the necessary measures in taking care of the city’s frontliners. <br><br>
+    
+    “We don’t want our healthcare workers to just sleep in the hospital corridors after long shifts, as they can’t go home and no public transportation.  We want to support their dedication in taking care of the COVID-19 patients, and for that, we want them to have safe, clean and a nice place to stay, and this is our small way to help them, they are truly our heroes,” Ms. Sue Geminiano, Marketing Manager of Hotel Sogo said. <br><br>
+    
+    Hotel Sogo is closely monitoring the COVID-19 outbreak as the management makes sure and strictly implements firm measures and enhanced cleaning protocols to prevent the transmission of the said virus. All employees and guests should pass the disinfection tent and all are subject to body temperature checks before entering the hotel.  Also, hand sanitizers have been placed in common areas and information dissemination has been rolled out as well. Social distancing steps are also being taken seriously to prevent the spread of the virus. <br><br>
+    
+    Truly, Hotel Sogo lives up to its so clean…so good state maintaining the cleanliness and providing suitable accommodation to all frontliners making it their home away from home. To find out more about Hotel Sogo, visit its website at www.hotelsogo.com or like and follow us on Facebook page @HotelSogoOfficialPage, Twitter and Instagram @hotelsogo, and Youtube account @OfficialHotelSogo.
+    ';
+    $image1 = '../assets/news/Provide_60M_worth/1.jpg';
+    $image2 = '../assets/news/Provide_60M_worth/2.jpg';
+}else if($title == 4){
+    $header = 'Hotel Sogo provides immediate help after Taal Volcano eruption';
+    $content = 'Sogo Cares, the Corporate Social Responsibility Program of Hotel Sogo, headed immediately to the south last January 15 to provide aid such as blankets, pillows, bath towels, new clothes, and mineral waters to more than 1,200 displaced families of Taal Volcano in Batangas.<br><br>
+ 
+    Sogo face masks were also distributed to protect affected residents from harmful emissions of the Taal Volcano, which is now on alert level 4. .<br><br>
+    
+    In coordination with the AFP Malasakit in Humanity, Philippine Army, Philippine Navy, and Philippine Air Force, Hotel Sogo’s Sogo Cares program conducted a one whole day caravan to bring all the needed assistance in seven evacuation sites in Lipa City and Municipality of Rosario in Batangas. .<br><br>
+     
+    “Aside from its 24/7 hotel services, Hotel Sogo is also working around the clock in this time of crisis. We immediately deployed our very own Sogo medical bus in coordination with the AFP to bring our relief goods for our fellow kababayans in Batangas,” said Ms. Sue Geminiano, Marketing Manager of Hotel Sogo. .<br><br>
+     
+    After 40 years of dormancy, the Taal Volcano has started spewing magma and hot ash last January 12, forcing thousands of people in Batangas province to evacuate. .<br><br>
+     
+    Since the calamity arises, Sogo Cares already helped different private groups and NGOs to augment the needed supply of relief goods. Another 1,000 blankets, bath towels, and new clothes were also donated to the Philippine Red Cross Batangas Chapter. .<br><br>
+    
+    Apart from the 1-day activity with the AFP, Navy, and Air Force, Sogo Cares continues to assist and help the people affected by the calamity. To date, there are already 20 barangays and 10,000 families that benefitted from the Sogo Cares program. .<br><br>
+     
+    To find out more about this and other Sogo Cares activities, like and follow us on Facebook page @sogocares and Instagram @sogo.cares.
+    ';
+    $image1 = '../assets/news/taal/1.jpg';
+    $image2 = '../assets/news/taal/2.jpg';
+}else if($title == 5){
+    $header = 'Hotel Sogo: A hotel like no other';
+    $content = 'For almost four months that the Philippines has been hit by a dreaded disease, the country has witnessed various events from the rising numbers of COVID-19 confirmed cases, death tolls, recovered patients, the heroism of frontliners, lockdown implementations, different situations of every Filipino family, and many more. <br><br>
+
+    We have recognized the full meaning of solidarity from the smallest act of staying at home up to saving and sacrificing the lives of our frontliners, and the act of sharing by ordinary citizens and biggest companies in the country. <br><br>
+    
+    Many big companies have taken the extra mile not only for the welfare of their employees but as well as for sharing a timely and efficient contribution to the community. Hotel Sogo, one of the leading lodging chains in the country is working around the clock to shelter our healthcare frontliners amidst the crisis. <br><br>
+    
+    Hotel Sogo continuously protecting the ones who are protecting us. <br><br>
+    
+    
+    <strong>More than just a slogan</strong><br><br>
+    
+     
+    Donation of relief goods to stranded Indigenous People (IP) in the metro in coordination with National Commission on Indigenous Peoples (NCIP) due of the pandemic. <br><br>
+    
+    
+    “So Clean, So Good!” The famous slogan that Hotel Sogo has been upholding over the years of providing top-notched services and amenities to its guests. <br><br>
+    
+    Not known to many, alongside this tagline is their commitment and compassion to give back to the community through its “Sogo Cares” under their corporate social responsibility program. Hotel Sogo never fails to deliver timely services to help the indigent communities and fellow Filipinos who are in need. <br><br>
+    
+    Since week one of the community quarantine, Hotel Sogo has donated P80-million worth of room accommodations to our medical frontliners. In coordination with 25 hospitals and mayors from different key cities, more than 800 rooms were provided, for free, to the doctors, nurses, medical technicians, and other healthcare providers in the country. To date, Hotel Sogo served as the home of the frontliners as it now accommodates also our fellow overseas Filipino workers and seafarer repatriates. <br><br>
+    
+    Series of donation activities in collaboration with other NGOs and local barangays have been conducted amidst the ECQ. <br><br>
+    
+    
+    
+    <strong>The game-changer</strong><br><br>
+    
+    Hotel Sogo unveiled new measures to re-establish confidence in the hotel industry as they adapt to the “new normal” state. <br><br>
+    
+    As part of their “So Clean, So Good, So Safe” campaign under its Sogo So Safe Program, Hotel Sogo continuously achieving meaningful action plans by optimizing hotel protocols across its branches nationwide. This immediate response was established to fully secure the safety of its employees especially all the frontliners, OFWs, and seafarer repatriates who are currently sheltering in their hotels. 
+    ';
+    $image1 = '../assets/news/Hotel_Like_no_other/1.jpg';
+    $image2 = '../assets/news/Hotel_Like_no_other/2.jpg';
 }
  
 
 ?>
 
-<a href="<?php echo base_url(); ?>news"><button class="btn btn-primary">Go Back</button></a>
+<a href="<?php echo base_url(); ?>news"><button class="btn btn-primary" style="color:  white; background-color: #573a1a; border:  #573a1a;">Go Back</button></a>
 
 <div class="newsPage">
     <div class="row">
@@ -53,8 +132,8 @@ if($title == 1){
             <p><?php echo $content; ?></p>
         </div>
         <div class="col-sm-3">
-            <img class="d-block img-fluid" src="<?php echo $image; ?>" alt="See More">
-            <img class="d-block img-fluid" src="<?php echo $image; ?>" alt="See More">
+            <img class="d-block img-fluid" src="<?php echo $image1; ?>" alt="See More">
+            <img class="d-block img-fluid" src="<?php echo $image2; ?>" alt="See More">
         </div>
     </div>
 </div>
