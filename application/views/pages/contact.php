955b5bdd9a5a141fae7fdf128004196f4883371f
giddel05012 <giddel.lucion@yahoo.com>
2020-10-14 20:28:52 +0800

    added link on google map

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -4,7 +4,9 @@
 <div class="content-wrapper">
     <div class="row contact">
         <div class="col-sm-6 left">
-            <img class="img-fluid" src="./assets/contact/map_contactus.jpg" alt="See More">
+            <a href="https://www.google.com/maps/place/629+Epifanio+de+los+Santos+Ave,+Cubao,+Quezon+City,+1111+Metro+Manila/@14.620851,121.0479843,17z/data=!3m1!4b1!4m5!3m4!1s0x3397b7c751278afd:0xa87dba2f4edd3817!8m2!3d14.620851!4d121.050173?hl=en" target="_blank">
+                <img class="img-fluid" src="./assets/contact/map_contactus.jpg" alt="See More">
+            </a>
         </div>
         <div class="col-sm-6">
             <form action="<?php echo base_url(); ?>sendEmail" method="post" accept-charset="utf-8">
