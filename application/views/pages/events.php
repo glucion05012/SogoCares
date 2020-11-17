2dfa7161b03db3f283ed81bb7773a00ba83f00ac
giddel05012 <giddel.lucion@yahoo.com>
2020-11-17 18:11:42 +0800

    added events buspromo

--- application/views/pages/events.php
+++ application/views/pages/events.php
@@ -4,6 +4,12 @@
 <div class="content-wrapper">
     
     <div class="row eventImg">
+        <div class="col-sm-12">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdG06ktfQV3sVlQCxcSo_L0W1aXIsttxbwJXACrWZ4GI5Ds7A/viewform" target="_blank">
+            <img class="d-block img-fluid" style="width: 100%" src="./assets/events/buspromo_website2.jpg" alt="Announcements">
+            </a>
+        </div>
+        
         <div class="col-sm-4">
             <a href="https://www.facebook.com/sogocares" target="_blank">
                 <img class="d-block img-fluid" src="./assets/events/announcement.jpg" alt="Announcements">
